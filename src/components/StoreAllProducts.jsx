6a1e335a580928284aa453135af93d36bae90ab7
Shima Safari <shimasafarimg@gmail.com>
2025-01-11 19:07:13 +0330

    add StoreAllProducts comp in Store

---
+++ src/components/StoreAllProducts.jsx
@@ -0,0 +1,99 @@
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import { Data } from "../assets/DataAssets";
+import ProductItem from "../components/ProductItem";
+import Grid from "@mui/material/Grid2";
+import { Typography } from "@mui/material";
+const StoreAllProducts = () => {
+  const { Products } = useContext(ShopContext);
+  const [PopularProducts, setPopularProduct] = useState([]);
+  useEffect(() => {
+    setPopularProduct(Products);
+  }, []);
+
+  return (
+    // Grid Container for 8 col bg white
+    <Grid
+      container
+      size={8}
+      sx={{
+        backgroundColor: "#FFFCF9",
+        borderRadius: "50px",
+        maxHeight: "2000px",
+      }}
+    >
+      {/*  Grid Products text All Products */}
+      <Grid
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          width: "100%",
+          alignItems: "center",
+          paddingY: "40px",
+        }}
+      >
+        <Grid
+          container
+          sx={{
+            display: "flex",
+            gap: 2,
+            paddingX: 7,
+            marginBottom: -1,
+          }}
+        >
+          <Typography
+            sx={{
+              fontSize: { md: 40, sm: 30 },
+              fontStyle: "normal",
+              fontWeight: 700,
+              lineHeight: "normal",
+            }}
+          >
+            All Products
+          </Typography>
+        </Grid>
+        <Grid
+          container
+          size={{ xs: 8, sm: 7, md: 6 }}
+          sx={{ justifyContent: "center" }}
+        >
+          <img src={Data.i_Line_Orange} alt="line_orange" width="90%" />
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        size={11.7}
+        rowSpacing={5}
+        sx={{
+          maxHeight: "1200px",
+          overflow: "auto",
+          scrollbarWidth: "thin",
+          marginBottom: "40px",
+        }}
+      >
+        {PopularProducts.map((item, index) => (
+          <Grid
+            container
+            size={{ lg: 4, md: 6, xs: 12 }}
+            sx={{
+              flexDirection: "row",
+              justifyContent: "center",
+            }}
+          >
+            <ProductItem
+              key={index}
+              id={item.id}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+              discountPrice={item.discountPrice}
+              discountPercent={item.discountPercent}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default StoreAllProducts;
--- src/pages/Store.jsx
+++ src/pages/Store.jsx
@@ -1,81 +1,10 @@
-import React, { useContext, useEffect, useState } from "react";
-import { ShopContext } from "../context/ShopContext";
-import { Data } from "../assets/DataAssets";
-import ProductItem from "../components/ProductItem";
+import React from "react";
+import StoreAllProducts from "../components/StoreAllProducts";
 import Grid from "@mui/material/Grid2";
-import { Box, Typography } from "@mui/material";
 const Store = () => {
-  const { Products } = useContext(ShopContext);
-  const [PopularProducts, setPopularProduct] = useState([]);
-  useEffect(() => {
-    setPopularProduct(Products);
-  }, []);
   return (
-    <Grid container>
-      <Grid
-        container
-        size={8}
-        sx={{ backgroundColor: "#FFFCF9", borderRadius: "50px" }}
-      >
-        {/*  Grid Products text */}
-        <Grid
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            width: "100%",
-            alignItems: "center",
-            paddingY: "40px",
-          }}
-        >
-          <Grid
-            container
-            sx={{
-              display: "flex",
-              gap: 2,
-              paddingX: 7,
-              marginBottom: -1,
-            }}
-          >
-            <Typography
-              sx={{
-                fontSize: 40,
-                fontStyle: "normal",
-                fontWeight: 700,
-                lineHeight: "normal",
-              }}
-            >
-              All Products
-            </Typography>
-          </Grid>
-          <Box>
-            <img src={Data.i_Line_Orange} alt="" />
-          </Box>
-        </Grid>
-        <Grid container size={12}>
-          <Grid
-            container
-            size={4}
-            sx={{
-              display: "flex",
-              flexDirection: "row",
-              gap: 1,
-              justifyContent: "space-evenly",
-            }}
-          >
-            {PopularProducts.map((item, index) => (
-              <ProductItem
-                key={index}
-                id={item.id}
-                image={item.image}
-                name={item.name}
-                price={item.price}
-                discountPrice={item.discountPrice}
-                discountPercent={item.discountPercent}
-              />
-            ))}
-          </Grid>
-        </Grid>
-      </Grid>
+    <Grid container sx={{ paddingX: { md: "70px", sm: "10px" },}}>
+      <StoreAllProducts />
     </Grid>
   );
 };
